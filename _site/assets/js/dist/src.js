b14f9880a9801a3f33bd7461eb1ae54fca387395
Micah Barrett <mbarrett02@risd.edu>
2014-07-08 15:24:49 -0400

    nav adjustments

--- _site/assets/js/dist/src.js
+++ _site/assets/js/dist/src.js
@@ -1,16 +1,44 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// write JavaScript here
-$('.drawer-activator').click(function(){
-	$('nav').toggleClass('drawer');
-})
-
-$(window).scroll(function(){
-	if($(window).scrollTop() > 30){
-		$('.logo').addClass('shrink');
-	} else if ($(window).scrollTop()< 10){
-		$('.logo').removeClass('shrink');
-	}
-})
 
 console.log($('p').text());
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
 },{}]},{},[1])
--- _site/assets/js/src/index.js
+++ _site/assets/js/src/index.js
@@ -1,14 +1,42 @@
-// write JavaScript here
-$('.drawer-activator').click(function(){
-	$('nav').toggleClass('drawer');
-})
-
-$(window).scroll(function(){
-	if($(window).scrollTop() > 30){
-		$('.logo').addClass('shrink');
-	} else if ($(window).scrollTop()< 10){
-		$('.logo').removeClass('shrink');
-	}
-})
 
 console.log($('p').text());
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
